a385efb5290734acc509e6ceebd2ce2ea92673f9
David L Bueno <davidlbueno@gmail.com>
2023-12-03 20:42:24 -0500

    Marker selection working

--- www/map.js
+++ www/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map', { minZoom: 2, maxZoom: 19 }).setView([0, 0], 4);
+var map = L.map('map', { minZoom: 2, maxZoom: 19 }).setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
