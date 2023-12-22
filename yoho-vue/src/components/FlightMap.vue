6d29671b51c16bfec8cf419f0f28f4a7bb71b899
David L Bueno <davidlbueno@gmail.com>
2023-12-21 23:49:33 -0500

    WIP adding markers...

--- yoho-vue/src/components/FlightMap.vue
+++ yoho-vue/src/components/FlightMap.vue
@@ -1,23 +1,62 @@
 <template>
-    <div id="FlightMap" style="height: 500px;"></div>
+  <div id="flightMap" style="height: 500px;"></div>
 </template>
-  
+
 <script>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default {
   name: 'FlightMap',
+  data() {
+    return {
+      map: null,
+      markers: {},
+    };
+  },
   mounted() {
-    const map = L.map('FlightMap').setView([51.505, -0.09], 13);
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    }).addTo(map);
-  }
+    this.initMap();
+    this.fetchAndPlotAirports();
+  },
+  methods: {
+    initMap() {
+      this.map = L.map('flightMap').setView([51.505, -0.09], 13);
+      L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, © <a href="https://stadiamaps.com/">Stadia Maps</a>'
+      }).addTo(this.map);
+    },
+    fetchAndPlotAirports() {
+      fetch('http://localhost:3000/flights')
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(flight => {
+            if (flight.originAirport) {
+              this.addMarker(flight.originAirport);
+            }
+            if (flight.destinationAirport) {
+              this.addMarker(flight.destinationAirport);
+            }
+          });
+        })
+        .catch(error => console.error('Error:', error));
+    },
+    addMarker(airport) {
+      if (!airport || !airport.iata_code || this.markers[airport.iata_code]) {
+        return;
+      }
+
+      const latLng = L.latLng(airport.latitude, airport.longitude);
+      const marker = L.marker(latLng).addTo(this.map)
+        .bindPopup(`<b>${airport.name}</b><br>${airport.city}, ${airport.country}`);
+
+      this.markers[airport.iata_code] = marker;
+    },
+  },
 };
 </script>
 
 <style>
-#map {
+#flightMap {
   height: 100%;
 }
 </style>
