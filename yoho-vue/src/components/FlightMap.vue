e063783229511705d76eb5a6a7af4e16f6e2a2d4
David L Bueno <davidlbueno@gmail.com>
2023-12-21 23:38:44 -0500

    CP dark leaflet map

--- yoho-vue/src/components/FlightMap.vue
+++ yoho-vue/src/components/FlightMap.vue
@@ -1,26 +1,23 @@
 <template>
-    <div id="map" style="height: 500px;"></div>
-  </template>
+    <div id="FlightMap" style="height: 500px;"></div>
+</template>
   
-  <script>
-  import L from 'leaflet';
-  import 'leaflet/dist/leaflet.css';
-  
-  export default {
-    name: 'FlightMap',
-    mounted() {
-      const map = L.map('map').setView([51.505, -0.09], 13);
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-      }).addTo(map);
-    }
-  };
-  </script>
-  
-  <style>
-  /* Make sure the map height is set */
-  #map {
-    height: 100%;
+<script>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+  name: 'FlightMap',
+  mounted() {
+    const map = L.map('FlightMap').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    }).addTo(map);
   }
-  </style>
-  
+};
+</script>
+
+<style>
+#map {
+  height: 100%;
+}
+</style>
