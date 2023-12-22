c17c8b9ef778fda7fcfa50312aca23d0eae1797f
David L Bueno <davidlbueno@gmail.com>
2023-12-22 15:59:58 -0500

    CP New Vue from scratch

---
+++ vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <FlightData/>
+  </div>
+</template>
+
+<script>
+import FlightData from './components/FlightMap.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FlightData
+  }
+};
+</script>
---
+++ vue/src/components/FlightMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Flight Data</h1>
+    <ul v-if="flights.length">
+      <li v-for="flight in flights" :key="flight._id">
+        {{ flight.origin }} - {{ flight.destination }}
+      </li>
+    </ul>
+    <p v-else>Loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlightData',
+  data() {
+    return {
+      flights: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/flights')
+      .then(response => response.json())
+      .then(data => {
+        this.flights = data;
+      })
+      .catch(error => console.error('Error fetching flights:', error));
+  },
+};
+</script>
