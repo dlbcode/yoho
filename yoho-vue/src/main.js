5ad66a5032654402a9d8b87ef2d66819f9a86a93
David L Bueno <davidlbueno@gmail.com>
2023-12-22 22:04:25 -0500

    CP Remove Vue  Folders

--- yoho-vue/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
-import './css/style.css';
-import './css/map.css';
-
-createApp(App).mount('#app')
