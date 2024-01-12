84087faa109f3e7a8513c0d91409c2c1cba049a6
David L Bueno <davidlbueno@gmail.com>
2024-01-11 22:19:17 -0500

    CP regular lines for world copies

--- www/js/pathDrawing.js
+++ www/js/pathDrawing.js
@@ -68,43 +68,9 @@ const pathDrawing = {
     createRoutePath(origin, destination, route) {
         let routeId = `${route.originAirport.iata_code}-${route.destinationAirport.iata_code}`;
     
-        const addDecoratedLine = (geodesicLine) => { // Function to create and add a decorated line
-            var planeIcon = L.icon({
-                iconUrl: '../assets/plane_icon.png', // Adjust the path as needed
-                iconSize: [16, 16],
-                iconAnchor: [8, 12]
-            });
-    
-            var planeSymbol = L.Symbol.marker({
-                rotate: true,
-                markerOptions: {
-                    icon: planeIcon
-                }
-            });
-    
-            var decoratedLine = L.polylineDecorator(geodesicLine, {
-                patterns: [
-                    {offset: '50%', repeat: 0, symbol: planeSymbol}
-                ]
-            }).addTo(map);
-    
-            return decoratedLine;
-        };
-    
-        if (this.routePathCache[routeId]) { // Check if the route is in the cache
-            this.routePathCache[routeId].forEach(path => { // Add each path in the cache to the map if it's not already there
-                if (!map.hasLayer(path)) {
-                    path.addTo(map);
-                }
-            });
-    
-            if (route.isDirect && this.routePathCache[routeId].length === 1) { // If the route is direct and not already decorated, add a decorated line
-                let decoratedLine = addDecoratedLine(this.routePathCache[routeId][0]);
-                this.routePathCache[routeId].push(decoratedLine);
-            }
-        } else { // Create and draw the geodesic line
-            const adjustedOrigin = L.latLng(origin.latitude, origin.longitude);
-            const adjustedDestination = L.latLng(destination.latitude, destination.longitude);
+        const drawPath = (origin, destination, offset) => {
+            const adjustedOrigin = L.latLng(origin.latitude, origin.longitude + offset);
+            const adjustedDestination = L.latLng(destination.latitude, destination.longitude + offset);
     
             var geodesicLine = new L.Geodesic([adjustedOrigin, adjustedDestination], {
                 weight: 1,
@@ -114,16 +80,53 @@ const pathDrawing = {
                 zIndex: -1
             }).addTo(map);
     
-            let newPaths = [geodesicLine]; // Store the newly created geodesic line
+            return geodesicLine;
+        };
     
-            if (route.isDirect) { // Add the decorated line for direct routes
-                let decoratedLine = addDecoratedLine(geodesicLine);
+        if (this.routePathCache[routeId]) {
+            this.routePathCache[routeId].forEach(path => {
+                if (!map.hasLayer(path)) {
+                    path.addTo(map);
+                }
+            });
+        } else {
+            let newPaths = [];
+            const worldCopies = [-720, -360, 0, 360, 720];
+            worldCopies.forEach(offset => {
+                newPaths.push(drawPath(origin, destination, offset));
+            });
+    
+            if (route.isDirect) {
+                let decoratedLine = this.addDecoratedLine(newPaths[2]); // Decorate the central line
                 newPaths.push(decoratedLine);
             }
     
-            this.routePathCache[routeId] = newPaths; // Add the newly created line(s) to the routePathCache
+            this.routePathCache[routeId] = newPaths;
         }
-    },             
+    },
+    
+    addDecoratedLine(geodesicLine) {
+        var planeIcon = L.icon({
+            iconUrl: '../assets/plane_icon.png',
+            iconSize: [16, 16],
+            iconAnchor: [8, 12]
+        });
+    
+        var planeSymbol = L.Symbol.marker({
+            rotate: true,
+            markerOptions: {
+                icon: planeIcon
+            }
+        });
+    
+        var decoratedLine = L.polylineDecorator(geodesicLine, {
+            patterns: [
+                {offset: '50%', repeat: 0, symbol: planeSymbol}
+            ]
+        }).addTo(map);
+    
+        return decoratedLine;
+    },                 
 
     drawLines() { // Iterate through each pair of consecutive waypoints
         for (let i = 0; i < appState.waypoints.length - 1; i++) {
