5180bece3b1c86f8b986601464b4c68b1eea4d04
David L Bueno <davidlbueno@gmail.com>
2024-04-16 16:55:47 -0400

    WIP 3

--- www/js/routeTable/routeInfoRow.js
+++ www/js/routeTable/routeInfoRow.js
@@ -27,62 +27,10 @@ function routeInfoRow(flight, rowElement, routeIndex) {
 
     const selectRouteButton = detailCell.querySelector('#selectRoute');
     selectRouteButton.addEventListener('click', () => {
-        const routeIds = flight.id.split('|');
-        const fullFlightData = flight;
 
-        console.log('routeIds:', routeIds);
-        console.log('fullFlightData:', fullFlightData);
-    
-       // Determine the group ID for the newly selected route
-       appState.highestGroupId += 1;
-       let newRouteGroupId = appState.highestGroupId;
-       const existingRouteDetails = appState.selectedRoutes[routeIndex];
-       if (existingRouteDetails) {
-           // Logic to remove routes from the old group, if necessary
-           Object.keys(appState.selectedRoutes).forEach(key => {
-               if (appState.selectedRoutes[key].group == existingRouteDetails.group) {
-                   updateState('removeSelectedRoute', parseInt(key));
-               }
-           });
-       }
+      // row click functionality to go here
 
-       // Update appState for the selected route
-       routeIds.forEach((id, idx) => {
-           const segmentData = fullFlightData.route[idx];
-           const departureDate = new Date(segmentData.local_departure).toISOString().split('T')[0];
-           const displayData = {
-               departure: new Date(segmentData.local_departure).toLocaleString(),
-               arrival: new Date(segmentData.local_arrival).toLocaleString(),
-               price: `$${fullFlightData.price}`,
-               airline: segmentData.airline,
-               stops: fullFlightData.route.length - 1,
-               route: `${segmentData.flyFrom} > ${segmentData.flyTo}`,
-               deep_link: fullFlightData.deep_link,
-           };
-
-           const selectedRouteIndex = routeIndex + idx;
-           if (!appState.routeDates[selectedRouteIndex]) {
-               appState.routeDates[selectedRouteIndex] = departureDate;
-           }
-
-           if (appState.selectedRoutes.hasOwnProperty(selectedRouteIndex)) {
-             const keys = Object.keys(appState.selectedRoutes).map(Number).filter(key => key >= selectedRouteIndex).sort((a, b) => b - a); // Sort keys in descending order to avoid overwriting
-             keys.forEach(key => {
-                 appState.selectedRoutes[key + 1] = appState.selectedRoutes[key]; // Shift each route up by one index
-             });
-         }
-         
-         appState.selectedRoutes[selectedRouteIndex] = {
-             displayData: displayData,
-             fullData: segmentData,
-             group: newRouteGroupId !== null ? newRouteGroupId : routeIndex,
-             routeDates: departureDate,
-         };
-       updateState('updateRouteDate: ', routeIndex, departureDate);
-       updateState('changeView', 'selectedRoute');
-       //highlightSelectedRowForRouteIndex(routeIndex);
-      });
     });
-}
+} 
 
 export { routeInfoRow };
