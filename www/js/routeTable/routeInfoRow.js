95aa4cca3d041e6b8472ecf83b3e5c305f4c3a03
David L Bueno <davidlbueno@gmail.com>
2024-04-21 13:35:20 -0400

    CP routeInfoRow formatting improvments

--- www/js/routeTable/routeInfoRow.js
+++ www/js/routeTable/routeInfoRow.js
@@ -7,7 +7,7 @@ function formatLayover(flight, idx) {
         const arrivalTime = new Date(flight.route[idx].local_arrival);
         const departureTime = new Date(flight.route[idx + 1].local_departure);
         const layoverDuration = (departureTime - arrivalTime) / 1000 / 60;  // convert from ms to minutes
-        return `<div>Layover: ${Math.floor(layoverDuration / 60)}h ${layoverDuration % 60}m</div>`;
+        return `${Math.floor(layoverDuration / 60)}h ${layoverDuration % 60}m`;
     }
     return '';
 }
@@ -33,9 +33,11 @@ function routeInfoRow(rowElement, fullFlightData, routeIds, routeIndex) {
     detailCell.innerHTML = `
     <div class='route-details' style='display: flex; flex-wrap: wrap; align-items: flex-start;'>
     <div>Route Details for ${flight.airlines.join(", ")}:</div>
-    ${flight.route.reduce((acc, segment, idx) => {
-        let nextSegment = flight.route[idx + 1];
+    ${flight.route.reduce((acc, segment, idx, arr) => {
+        let nextSegment = arr[idx + 1];
         let isIntermediary = nextSegment && segment.flyTo === nextSegment.flyFrom;
+        let isFinalDestination = nextSegment && nextSegment.flyTo === arr[arr.length - 1].flyTo;
+
         if (isIntermediary) {
             acc.push(`<div class='segment-details' style='display: flex; flex-direction: row; align-items: center; margin-right: 20px;'>
                 <div style='margin-right: 10px;'>
@@ -51,33 +53,20 @@ function routeInfoRow(rowElement, fullFlightData, routeIds, routeIndex) {
                     <div>Arrival: ${new Date(segment.local_arrival).toLocaleTimeString()}</div>
                     <div>Layover: ${formatLayover(flight, idx)}</div>
                     <div>Departure: ${new Date(nextSegment.local_departure).toLocaleTimeString()}</div>
-                </div>
-                <div style='margin-right: 10px;'>
-                    <div>&gt;</div>
-                    <div>Duration: ${((new Date(nextSegment.local_arrival) - new Date(nextSegment.local_departure)) / 3600000).toFixed(1)} hrs</div>
-                </div>
-                <div style='margin-right: 10px;'>
-                    <div>${nextSegment.flyTo}</div>
-                    <div>Arrival: ${new Date(nextSegment.local_arrival).toLocaleTimeString()}</div>
-                </div>
-            </div>`);
-            return acc;
-        } else if (!acc.find(item => item.includes(segment.flyFrom))) {
-            acc.push(`<div class='segment-details' style='display: flex; flex-direction: row; align-items: center; margin-right: 20px;'>
-                <div style='margin-right: 10px;'>
-                    <div>${segment.flyFrom}</div>
-                    <div>Departure: ${new Date(segment.local_departure).toLocaleTimeString()}</div>
-                </div>
-                <div style='margin-right: 10px;'>
-                    <div>&gt;</div>
-                    <div>Duration: ${((new Date(segment.local_arrival) - new Date(segment.local_departure)) / 3600000).toFixed(1)} hrs</div>
-                </div>
-                <div style='margin-right: 10px;'>
-                    <div>${segment.flyTo}</div>
-                    <div>Arrival: ${new Date(segment.local_arrival).toLocaleTimeString()}</div>
-                </div>
+                </div>`);
+        }
+
+        if (isFinalDestination) {
+            acc.push(`<div style='margin-right: 10px;'>
+                <div>&gt;</div>
+                <div>Duration: ${((new Date(nextSegment.local_arrival) - new Date(nextSegment.local_departure)) / 3600000).toFixed(1)} hrs</div>
+            </div>
+            <div style='margin-right: 10px;'>
+                <div>${nextSegment.flyTo}</div>
+                <div>Arrival: ${new Date(nextSegment.local_arrival).toLocaleTimeString()}</div>
             </div>`);
         }
+
         return acc;
     }, []).join('')}
     </div>
