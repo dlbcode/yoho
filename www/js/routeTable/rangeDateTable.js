154278a5a1f8563e1e2f4246dc17e71ce05a6f6c
David L Bueno <davidlbueno@gmail.com>
2024-05-02 08:26:57 -0400

    CP filtering working for rangeDateTable

--- www/js/routeTable/rangeDateTable.js
+++ www/js/routeTable/rangeDateTable.js
@@ -17,9 +17,9 @@ function buildDateRangeTable(routeIndex, dateRange) {
 
   document.head.appendChild(Object.assign(document.createElement('link'), {rel: 'stylesheet', type: 'text/css', href: '../css/routeTable.css'}));
 
-   // Start the loading animation
-   const topBar = document.getElementById('top-bar');
-   topBar.classList.add('loading');
+  // Start the loading animation
+  const topBar = document.getElementById('top-bar');
+  topBar.classList.add('loading');
 
   const origin = currentRoute.originAirport.iata_code;
   const destination = currentRoute.destinationAirport.iata_code;
@@ -32,35 +32,34 @@ function buildDateRangeTable(routeIndex, dateRange) {
   }
 
   fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Failed to fetch route data: ${response.statusText}`);
-    }
-    return response.json();
-  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch route data: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const table = document.createElement('table');
+      table.className = 'route-info-table';
+      table.style.width = '100%';
+      table.setAttribute('data-route-index', routeIndex, 'border', '1');
 
-  .then(data => {
-    const table = document.createElement('table');
-    table.className = 'route-info-table';
-    table.style.width = '100%';
-    table.setAttribute('data-route-index', routeIndex, 'border', '1');
-
-    const thead = document.createElement('thead');
-    let headerRow = `<tr>
-                        <th>Departure <span class="sortIcon" data-column="departure">&#x21C5;</span><img class="filterIcon" data-column="departure" src="/assets/filter-icon.svg" alt="Filter"></th>
-                        <th>Arrival <span class="sortIcon" data-column="arrival">&#x21C5;</span><img class="filterIcon" data-column="arrival" src="/assets/filter-icon.svg" alt="Filter"></th>
-                        <th>Price <span class="sortIcon" data-column="price">&#x21C5;</span><img id="priceFilter" class="filterIcon" data-column="price" src="/assets/filter-icon.svg" alt="Filter"></th>
-                        <th>Airlines <span class="sortIcon" data-column="airlines">&#x21C5;</span></th>
-                        <th>Direct <span class="sortIcon" data-column="direct">&#x21C5;</span></th>
-                        <th>Stops <span class="sortIcon" data-column="stops">&#x21C5;</span></th>
-                        <th>Layovers <span class="sortIcon" data-column="layovers">&#x21C5;</span></th>
-                        <th>Duration <span class="sortIcon" data-column="duration">&#x21C5;</span></th>
-                        <th>Route <span class="sortIcon" data-column="route">&#x21C5;</span></th>
-                     </tr>`;
+      const thead = document.createElement('thead');
+      let headerRow = `<tr>
+                    <th data-column="departure">Departure <span class="sortIcon" data-column="departure">&#x21C5;</span><img class="filterIcon" id="departureFilter"data-column="departure" src="/assets/filter-icon.svg" alt="Filter"></th>
+                    <th data-column="arrival">Arrival <span class="sortIcon" data-column="arrival">&#x21C5;</span><img id="arrivalFilter" class="filterIcon" data-column="arrival" src="/assets/filter-icon.svg" alt="Filter"></th>
+                    <th data-column="price"><span id="priceText">Price</span><span class="sortIcon" data-column="price">&#x21C5;</span><img id="priceFilter" class="filterIcon" data-column="price" src="/assets/filter-icon.svg" alt="Filter"></th>
+                    <th data-column="airlines">Airlines <span class="sortIcon" data-column="airlines">&#x21C5;</span></th>
+                    <th data-column="direct">Direct <span class="sortIcon" data-column="direct">&#x21C5;</span></th>
+                    <th data-column="stops">Stops <span class="sortIcon" data-column="stops">&#x21C5;</span></th>
+                    <th data-column="layovers">Layovers <span class="sortIcon" data-column="layovers">&#x21C5;</span></th>
+                    <th data-column="duration">Duration <span class="sortIcon" data-column="duration">&#x21C5;</span></th>
+                    <th data-column="route">Route <span class="sortIcon" data-column="route">&#x21C5;</span></th>
+                 </tr>`;
       thead.innerHTML = headerRow;
-    table.appendChild(thead);
+      table.appendChild(thead);
 
-    const tbody = document.createElement('tbody');
+      const tbody = document.createElement('tbody');
 
     if (Array.isArray(data.data)) {
       data.data.forEach(flight => {
@@ -83,71 +82,67 @@ function buildDateRangeTable(routeIndex, dateRange) {
         const formattedArrival = `${arrivalDayName} ${arrivalDate.toLocaleString()}`;
     
         row.innerHTML = `<td>${formattedDeparture}</td>
-                          <td>${formattedArrival}</td>
-                          <td>$${price}</td>
-                          <td>${flight.airlines.join(", ")}</td>
-                          <td>${directFlight ? '✓' : ''}</td>
-                          <td>${stops}</td>
-                          <td>${layovers}</td>
-                          <td>${durationHours}h ${durationMinutes}m</td>
-                          <td>${routeIATAs}</td>`;
+                         <td>${formattedArrival}</td>
+                         <td>$${price}</td>
+                         <td>${flight.airlines.join(", ")}</td>
+                         <td>${directFlight ? '✓' : ''}</td>
+                         <td>${stops}</td>
+                         <td>${layovers}</td>
+                         <td>${durationHours}h ${durationMinutes}m</td>
+                         <td>${routeIATAs}</td>`;
         tbody.appendChild(row);
       });         
     } else {
       console.error('data.data is not an array:', data.data);
 
     }
-    table.appendChild(tbody);
-    infoPaneContent.appendChild(table);
+      table.appendChild(tbody);
+      infoPaneContent.appendChild(table);
 
     topBar.classList.remove('loading');
 
     pathDrawing.drawRouteLines();
 
-    highlightSelectedRowForRouteIndex(routeIndex);
-
-    attachEventListeners(table, data, routeIndex);
-  })
-  .catch(error => {
-    infoPaneContent.textContent = 'Error loading data: ' + error.message;
-  });  
-
-  function attachEventListeners(table, data, routeIndex) {
-    const headers = table.querySelectorAll('th');
-    headers.forEach(header => {
-      header.style.cursor = 'pointer';
-      header.addEventListener('click', function(event) {
-        if (!event.target.closest('.filterIcon')) {
-          const sortIcon = this.querySelector('.sortIcon');
-          const columnIdentifier = sortIcon.getAttribute('data-column');
-          const columnIndex = getColumnIndex(columnIdentifier);
-          const isAscending = sortIcon.getAttribute('data-sort') !== 'asc';
-          console.log('Sorting by column:', columnIdentifier, 'index:', columnIndex, 'ascending:', isAscending);
-          sortTableByColumn(table, columnIndex, isAscending);
-          resetSortIcons(headers, sortIcon, isAscending ? 'asc' : 'desc');
-        }
-      });
+      highlightSelectedRowForRouteIndex(routeIndex);
+      attachEventListeners(table, data, routeIndex);
+    })
+    .catch(error => {
+      infoPaneContent.textContent = 'Error loading data: ' + error.message;
     });
 
-    document.querySelectorAll('.filterIcon').forEach(icon => {
-      icon.addEventListener('click', function(event) {
-          event.stopPropagation();
-          const column = this.getAttribute('data-column');
-          if (!column) {
-              console.error('Column attribute is missing on the icon:', this);
-              return;
-          }
-          console.log('Filtering column:', column);
-          const data = fetchDataForColumn(column);
-          console.log('Data for column:', data)
-          if (data) {
-              sliderFilter.createFilterPopup(column, data, event);
-          } else {
-              console.error('Failed to fetch data for column:', column);
-          }
-      });
-    });
+    function attachEventListeners(table, data, routeIndex) {
+      const headers = table.querySelectorAll('th');
+      headers.forEach(header => {
+          header.style.cursor = 'pointer';
+          header.addEventListener('click', function(event) {
+              const sortIcon = event.target.closest('.sortIcon');
+              if (sortIcon) {  // This checks if the clicked element is the sortIcon itself
+                  const columnIdentifier = sortIcon.getAttribute('data-column');
+                  const columnIndex = getColumnIndex(columnIdentifier);
+                  const isAscending = sortIcon.getAttribute('data-sort') !== 'asc';
+                  sortTableByColumn(table, columnIndex, isAscending);
+                  resetSortIcons(headers, sortIcon, isAscending ? 'asc' : 'desc');
+              }
+          });
+      });  
 
+      document.querySelectorAll('.filterIcon').forEach(icon => {
+        icon.addEventListener('click', function(event) {
+            event.stopPropagation();
+            const column = this.getAttribute('data-column');
+            if (!column) {
+                console.error('Column attribute is missing on the icon:', this);
+                return;
+            }
+            const data = fetchDataForColumn(column);
+            if (data) {
+                sliderFilter.createFilterPopup(column, data, event);
+            } else {
+                console.error('Failed to fetch data for column:', column);
+            }
+        });
+    });
+ 
     function fetchDataForColumn(column) {
       switch (column) {
           case 'price':
@@ -156,50 +151,49 @@ function buildDateRangeTable(routeIndex, dateRange) {
                   const priceText = cell.textContent.replace(/[^\d.]/g, ''); // Remove any non-numeric characters, including the dollar sign
                   return parseFloat(priceText);
               }).filter(price => !isNaN(price)); // Ensure only valid numbers are included
-
+  
               if (prices.length === 0) {
                   console.error('No valid prices found in the column');
                   return { min: 0, max: 0 }; // Return default or error values if no prices are found
               }
-
+  
               const min = Math.min(...prices);
               const max = Math.max(...prices);
               return { min, max };
-
+  
           case 'departure':
           case 'arrival':
               return {
                   min: 0,
                   max: 24
               };
-
+  
           default:
               console.error('Unsupported column:', column);
               return null;
       }
-    }
-
-    document.querySelectorAll('.route-info-table tbody tr').forEach((row, index) => {
-      row.addEventListener('click', function() {
-        const routeIdString = this.getAttribute('data-route-id');
-        const routeIds = routeIdString.split('|');
-        const fullFlightData = data.data[index];
-        routeInfoRow(this, fullFlightData, routeIds, routeIndex);
-      });
+  }
+      document.querySelectorAll('.route-info-table tbody tr').forEach((row, index) => {
+        row.addEventListener('click', function() {
+          const routeIdString = this.getAttribute('data-route-id');
+          const routeIds = routeIdString.split('|');
+          const fullFlightData = data[index];
+          routeInfoRow(this, fullFlightData, routeIds, routeIndex);
+        });
     });    
     
     document.querySelectorAll('.route-info-table tbody tr').forEach(row => {
       row.addEventListener('mouseover', function() {
         const routeString = this.cells[8].textContent.trim();
         const iataCodes = routeString.split(' > ');
-
+  
         for (let i = 0; i < iataCodes.length - 1; i++) {
             const originIata = iataCodes[i];
             const destinationIata = iataCodes[i + 1];
             pathDrawing.drawPathBetweenAirports(originIata, destinationIata, flightMap.getAirportDataByIata);
         }
       });
-
+  
       row.addEventListener('mouseout', function() {
           pathDrawing.clearLines();
           pathDrawing.drawLines();
@@ -235,5 +229,5 @@ function buildDateRangeTable(routeIndex, dateRange) {
     return columnMap[columnIdentifier];
   }
 }
-    
-  export { buildDateRangeTable };
+
+export { buildDateRangeTable };
