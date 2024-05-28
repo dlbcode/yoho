e9adcd49d37857e5c6336e5fd4ba755da3fa4269
David L Bueno <davidlbueno@gmail.com>
2024-05-28 14:57:29 -0400

    WIP tripTypePicker fix

--- www/js/routeBox/tripTypePicker.js
+++ www/js/routeBox/tripTypePicker.js
@@ -29,8 +29,14 @@ export function tripTypePicker(routeNumber) {
         const listItem = document.createElement('li');
         listItem.textContent = type;
         listItem.addEventListener('click', () => {
+            if (!appState.routes[routeNumber]) {
+                appState.routes[routeNumber] = {};
+            }
+            if (!appState.routeTypes) {
+                appState.routeTypes = {};
+            }
             appState.routes[routeNumber].tripType = tripTypeValues[index];
-            appState.routeTypes[routeNumber] = tripTypeValues[index];
+            appState.routeTypes[routeNumber] = tripTypeValues[index]; // Line 36
             dropdownBtn.innerHTML = `${type} <span class="icon-dropdown"></span>`;
             dropdownList.classList.add('hidden');
             updateState('tripType', { routeNumber, tripType: tripTypeValues[index] });
@@ -38,7 +44,7 @@ export function tripTypePicker(routeNumber) {
             document.dispatchEvent(new CustomEvent('stateChange', { detail: { key: 'tripType', value: tripTypeValues[index] } }));
         });
         dropdownList.appendChild(listItem);
-    });
+    });        
 
     tripTypeContainer.appendChild(dropdownList);
 
