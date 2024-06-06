235dbc940b60de599ff48e9f8cbd34b8b9479d17
David L Bueno <davidlbueno@gmail.com>
2024-06-06 13:30:35 -0400

    WIP 2 adjust resizeHandle

--- www/js/uiHandling.js
+++ www/js/uiHandling.js
@@ -46,8 +46,8 @@ const uiHandling = {
 
         const clientY = e.touches ? e.touches[0].clientY : e.clientY;
         const newHeight = startHeight - (clientY - startY);
-        const maxHeight = window.innerHeight; // Adjust to leave space for OS menu
-        infoPane.style.height = `${Math.min(Math.max(80, newHeight), maxHeight)}px`;
+        const maxHeight = window.innerHeight - 100; // Adjust to leave space for OS menu
+        infoPane.style.height = `${Math.min(Math.max(140, newHeight), maxHeight)}px`;
         requestAnimationFrame(adjustMapSize);
     };
 
