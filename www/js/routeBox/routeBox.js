3400ed9b9d187c3ab97d269e61cd41c4d0ae369e
David L Bueno <davidlbueno@gmail.com>
2025-02-20 14:18:41 -0500

    rm closeButton from routeBox

--- www/js/routeBox/routeBox.js
+++ www/js/routeBox/routeBox.js
@@ -242,20 +242,6 @@ const routeBox = {
         return searchButton;
     },
 
-    createCloseButton(routeBoxElement, routeNumber) {
-        const closeButton = createElement('span', { className: 'popup-close-button', content: `
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 8L12 16L20 8" stroke="#666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>` });
-        closeButton.onclick = () => {
-            const fromInput = document.querySelector('.from-input input');
-            const toInput = document.querySelector('.to-input input');
-            if (!fromInput.value.trim() && !toInput.value.trim()) removeRoute(routeNumber);
-            routeBoxElement.style.display = 'none';
-        };
-        return closeButton;
-    },
-
     handleSwapButtonClick(routeNumber) {
         const inputs = document.querySelectorAll('.waypoint-inputs-container input[type="text"]');
         if (inputs.length === 2) {
