0560ee645b75e3b5d952210c39948cb52982ae7a
David L Bueno <davidlbueno@gmail.com>
2025-02-26 11:28:50 -0500

    WIP3 statemanger cleanup

--- www/js/stateManager.js
+++ www/js/stateManager.js
@@ -25,9 +25,12 @@ const appState = {
     filterThresholds: {},
 };
 
-const originalUpdateState = window.updateState;
-window.updateState = function(key, value, caller) {
-    // Special handling for waypoint removal
+function updateState(key, value, calledFrom) {
+    console.log(`updateState called from: ${calledFrom}, key: ${key}, value:`, value);
+    let shouldUpdateUrl = true;
+    let shouldContinue = true;
+
+    // Special handling for waypoint removal (moved from wrapper function)
     if (key === 'removeWaypoint') {
         const waypointIndex = value;
         const waypoint = appState.waypoints[waypointIndex];
@@ -40,117 +43,115 @@ window.updateState = function(key, value, caller) {
         
         // Skip the removal for "Any" destinations
         if (isAnyDestination) {
-            return;
+            console.log(`Preserving "Any" destination at index ${waypointIndex}`);
+            shouldContinue = false; // Don't proceed with the state change
         }
     }
-    
-    // Call the original function for normal operation
-    originalUpdateState.call(this, key, value, caller);
-};
 
-function updateState(key, value, calledFrom) {
-    console.log(`updateState called from: ${calledFrom}, key: ${key}, value:`, value);
-    let shouldUpdateUrl = true;
+    // Only proceed with state changes if we should continue
+    if (shouldContinue) {
+        switch (key) {
+            case 'routeDirection':
+                appState.routeDirection = value;
+                break;
 
-    switch (key) {
-        case 'routeDirection':
-            appState.routeDirection = value;
-            break;
+            case 'updateRouteDate':
+                const { routeNumber, depart, return: returnDate } = value;
+                const routeDate = appState.routeDates[routeNumber] || { depart: null, return: null };
+                if (routeDate.depart !== depart || routeDate.return !== returnDate) {
+                    appState.routeDates[routeNumber] = { depart: depart || routeDate.depart, return: returnDate ?? routeDate.return };
+                    Object.keys(appState.selectedRoutes).forEach(key => {
+                        if (parseInt(key) >= routeNumber && appState.selectedRoutes[key]) {
+                            appState.selectedRoutes[key].routeDates = { depart, return: returnDate ?? null };
+                        }
+                    });
+                }
+                break;
 
-        case 'updateRouteDate':
-            const { routeNumber, depart, return: returnDate } = value;
-            const routeDate = appState.routeDates[routeNumber] || { depart: null, return: null };
-            if (routeDate.depart !== depart || routeDate.return !== returnDate) {
-                appState.routeDates[routeNumber] = { depart: depart || routeDate.depart, return: returnDate ?? routeDate.return };
-                Object.keys(appState.selectedRoutes).forEach(key => {
-                    if (parseInt(key) >= routeNumber && appState.selectedRoutes[key]) {
-                        appState.selectedRoutes[key].routeDates = { depart, return: returnDate ?? null };
-                    }
+            case 'tripType':
+                appState.routes[value.routeNumber] = { ...appState.routes[value.routeNumber], tripType: value.tripType };
+                break;
+
+            case 'updateTravelers':
+                if (value.routeNumber != null && appState.routes[value.routeNumber]) {
+                    appState.routes[value.routeNumber].travelers = parseInt(value.travelers);
+                }
+                shouldUpdateUrl = false;
+                break;
+
+            case 'updateWaypoint':
+                if (value.index >= 0 && value.index < appState.waypoints.length) {
+                    appState.waypoints[value.index] = { ...appState.waypoints[value.index], ...value.data };
+                }
+                break;
+
+            case 'addWaypoint':
+                appState.waypoints.push(...(Array.isArray(value) ? value : [value]));
+                appState.isEditingWaypoint = true;
+                break;
+
+            case 'removeWaypoint':
+                if (value >= 0 && value < appState.waypoints.length) {
+                    appState.waypoints.splice(value, 1);
+                }
+                appState.isEditingWaypoint = false;
+                break;
+
+            case 'removeWaypoints':
+                appState.waypoints.splice(value.routeNumber * 2, 2);
+                break;
+
+            case 'updateRoutes':
+                appState.routes = value.map((route, index) => ({
+                    ...appState.routes[index],
+                    ...route,
+                    travelers: route.travelers || 1,
+                    tripType: route.tripType || appState.routes[index]?.tripType || 'oneWay'
+                }));
+                break;
+
+            case 'clearData':
+                Object.assign(appState, {
+                    waypoints: [],
+                    routes: [],
+                    trips: [],
+                    selectedRoutes: {},
+                    routeDates: {}
                 });
-            }
-            break;
+                break;
 
-        case 'tripType':
-            appState.routes[value.routeNumber] = { ...appState.routes[value.routeNumber], tripType: value.tripType };
-            break;
+            case 'updateSelectedRoute':
+                appState.selectedRoutes[value.routeIndex] = value.routeDetails;
+                appState.routeDates[value.routeIndex] = value.routeDetails.routeDates;
+                break;
 
-        case 'updateTravelers':
-            if (value.routeNumber != null && appState.routes[value.routeNumber]) {
-                appState.routes[value.routeNumber].travelers = parseInt(value.travelers);
-            }
-            shouldUpdateUrl = false;
-            break;
+            case 'selectedAirport':
+                appState.selectedAirport = value;
+                shouldUpdateUrl = false;
+                break;
 
-        case 'updateWaypoint':
-            if (value.index >= 0 && value.index < appState.waypoints.length) {
-                appState.waypoints[value.index] = { ...appState.waypoints[value.index], ...value.data };
-            }
-            break;
+            case 'removeSelectedRoute':
+                delete appState.selectedRoutes[value];
+                break;
 
-        case 'addWaypoint':
-            appState.waypoints.push(...(Array.isArray(value) ? value : [value]));
-            appState.isEditingWaypoint = true;
-            break;
+            case 'changeView':
+                appState.currentView = value;
+                break;
 
-        case 'removeWaypoint':
-            if (value >= 0 && value < appState.waypoints.length) {
-                appState.waypoints.splice(value, 1);
-            }
-            appState.isEditingWaypoint = false;
-            break;
+            default:
+                appState[key] = value;
+                break;
+        }
 
-        case 'removeWaypoints':
-            appState.waypoints.splice(value.routeNumber * 2, 2);
-            break;
+        document.dispatchEvent(new CustomEvent('stateChange', { detail: { key, value } }));
 
-        case 'updateRoutes':
-            appState.routes = value.map((route, index) => ({
-                ...appState.routes[index],
-                ...route,
-                travelers: route.travelers || 1,
-                tripType: route.tripType || appState.routes[index]?.tripType || 'oneWay'
-            }));
-            break;
-
-        case 'clearData':
-            Object.assign(appState, {
-                waypoints: [],
-                routes: [],
-                trips: [],
-                selectedRoutes: {},
-                routeDates: {}
-            });
-            break;
-
-        case 'updateSelectedRoute':
-            appState.selectedRoutes[value.routeIndex] = value.routeDetails;
-            appState.routeDates[value.routeIndex] = value.routeDetails.routeDates;
-            break;
-
-        case 'selectedAirport':
-            appState.selectedAirport = value;
-            shouldUpdateUrl = false;
-            break;
-
-        case 'removeSelectedRoute':
-            delete appState.selectedRoutes[value];
-            break;
-
-        case 'changeView':
-            appState.currentView = value;
-            break;
-
-        default:
-            appState[key] = value;
-            break;
-    }
-
-    document.dispatchEvent(new CustomEvent('stateChange', { detail: { key, value } }));
-
-    if (shouldUpdateUrl) {
-        updateUrl();
+        if (shouldUpdateUrl) {
+            updateUrl();
+        }
     }
 }
+
+window.updateState = updateState;
 
 function updateUrl() {
     const params = new URLSearchParams(window.location.search);
